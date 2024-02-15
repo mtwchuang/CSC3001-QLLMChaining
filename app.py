5d9f07d6435a42ae0ec4088d3606efdab9312167
Actarnix <85341741+mtwchuang@users.noreply.github.com>
2024-02-15 14:51:06 +0800

    ui edits

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ import db_config
 import pandas as pd
 import llm_config
 
-# # global variables
+# global variables
 # db_config.drop_db()
-# db_conn = db_config.connect_to_db()
+db_conn = db_config.connect_to_db()
 
-# # setting up sqlite3 database
-# books_df = pd.read_csv('datasets/books_data.csv')
-# db_config.create_table('TBL_Books', books_df)
-# test_context = db_config.get_db_schema()
+# setting up sqlite3 database
+books_df = pd.read_csv('raw_datasets/books_data.csv')
+db_config.create_table('TBL_Books', books_df)
+test_context = db_config.get_db_schema()
+
+# setting up llm
+nsql_llm = llm_config.deploy_nsql_llama()
+
 # question = "Which are the books which were published before the year 1875?"
-# nsql_llm = llm_config.deploy_nsql_llama()
 # generated_query = llm_config.generate_sql_query(nsql_llm, test_context, question)
 # print(generated_query)
 
@@ -21,7 +24,7 @@ import llm_config
 # db_cursor.execute(generated_query)
 # rows = db_cursor.fetchall()
 # for row in rows:
-#         print(row)
+#     print(row)
 
 # Streamlit Application Code
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -32,23 +35,37 @@ st.markdown("Using local language models to power information retrieval")
 
 # Streamlit Expander
 with st.expander("Try asking me these queries"):
-      st.write("""
-        \n > 1) Something 
-        \n > 2) Something
-        \n > 3) Something
-        """)      
+    st.write("""
+    \n > 1) Something 
+    \n > 2) Something
+    \n > 3) Something
+    """)      
 
 # Streamlit Sidebar Menu
 st.sidebar.caption(''':robot_face: Hello, I am a query assistant for your data. ''')
-st.sidebar.button("New Chat")
 
-# Streamlit Chat
-if "messages" not in st.session_state:
+# Initialize chat messages if not already present
+if "messages" not in st.session_state or st.sidebar.button("New Chat"):
     st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
-for msg in st.session_state.messages:    
+
+# Display chat messages
+for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-user_query = st.chat_input(placeholder="Ask me anything!")
+# Get user input
+user_question = st.chat_input(placeholder="Ask me anything!")
 
-# if "messages" not in st.session_state.keys():
-#       st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
+# Add user question to message chain
+if user_question:
+    st.session_state.messages.append({"role": "user", "content": user_question})
+    # Display the user's input correctly
+    st.chat_message("user").write(user_question)
+
+    with st.chat_message("assistant"):
+        try:
+            output_query = llm_config.generate_sql_query(nsql_llm, test_context, user_question)
+            st.write(output_query)
+        except Exception as error:
+            print("Error: ", error)
+    
+    
